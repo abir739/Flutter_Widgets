cee5a3fb95ad2b6b2e081f63a794920dbd415710
abir cherif <abir.cherif98@gmail.com>
2025-03-16 13:30:40 +0100

    Counter App using Riverpod

---
+++ Flutter State Management/Riverpod/counter_app_riverpod/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider<int>((ref) => 0);
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Riverpod ')),
+      ),
+      body: Center(
+        child: Consumer(builder: (context, ref, child) {
+          final count = ref.watch(counterProvider);
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('count: $count'),
+              ElevatedButton(
+                  onPressed: () => ref.read(counterProvider.notifier).state++,
+                  child: const Text('Increment'))
+            ],
+          );
+        }),
+      ),
+    ));
+  }
+}
